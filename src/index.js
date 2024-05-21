afa0448a661b210a2fea4566ab01be499f8a1bba
haganvrazel <vrazel97@yahoo.com>
2024-05-21 10:57:54 -0500

    Apollo Server Setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./resolvers');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+    console.log('Server is ready at ${url}');
+})

5ddd11c249a5693221877679edb0c0471de4db22
haganvrazel <vrazel97@yahoo.com>
2024-05-21 12:37:54 -0500

    removing this commit after test complete

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const resolvers = require('./resolvers');
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-    console.log('Server is ready at ${url}');
+    console.log(`Server is ready at ${url}`);
 })

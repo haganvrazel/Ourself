55575382f07962afe9fd263742212133a2acead1
haganvrazel <vrazel97@yahoo.com>
2024-05-21 12:27:55 -0500

    added placeholders for future extension for db config and model

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+// Placeholder for future configuration of db
+// Using MongoDB with Mongoose as an example
+/*
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+*/
---
+++ src/models/MenuItem.js
@@ -0,0 +1,25 @@
+// Placeholder for future model of db
+// Using MongoDB with Mongoose as an example
+/*
+const mongoose = require('mongoose');
+
+const MenuItemSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('MenuItem', MenuItemSchema);
+*/
